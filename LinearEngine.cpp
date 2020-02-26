722e7503436fa9999ba2aa64d716b1806e75326f
Muzi Gumede <Muzigumede@outlook.com>
2020-02-26 14:26:35 +0200

    initial testing

--- LinearEngine.cpp
+++ LinearEngine.cpp
@@ -1,4 +1,15 @@
 #include <iostream>
 using namespace std;
 
-int main(){}
+void printer(string seq){
+        cout << seq << endl;
+}
+
+int main(){
+        string equation = "";
+        cout << "Enter lineqr equation: " <<endl;
+        cin >> equation;
+        printer(equation);
+
+        return 0;
+}

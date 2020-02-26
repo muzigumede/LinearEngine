be7a7fb292c33e66157e6b7ab7e9f0ea29081383
Muzi Gumede <Muzigumede@outlook.com>
2020-02-26 15:12:22 +0200

    switched from strings to vector strings in f printer()

--- LinearEngine.cpp
+++ LinearEngine.cpp
@@ -1,15 +1,32 @@
 #include <iostream>
+#include <vector>
 using namespace std;
 
-void printer(string seq){
-        cout << seq << endl;
+
+void printer(std::vector<string> x){
+        for(int i=0;i<x.size();i++){
+                cout << x[i] << endl;
+        }
 }
 
 int main(){
-        string equation = "";
-        cout << "Enter lineqr equation: " <<endl;
-        cin >> equation;
-        printer(equation);
+        vector<string> equations;
+        string equation;
+
+        for(int i=0;i<10;i++){
+                cout << "Enter equation " + to_string(i + 1) + " / click Enter 0 to complete." << endl;
+                cin >> equation;
+                if(equation != "0"){
+                        equations.push_back(equation);
+                }else{
+                        break;
+                }
+        }
+        
+        printer(equations);
 
         return 0;
 }
+
+
+

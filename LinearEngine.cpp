06d38b19c48b52948c5749092006a5a37c4478ff
Muzi Gumede <Muzigumede@outlook.com>
2020-03-18 18:37:32 +0200

    remove global using namespace std

--- LinearEngine.cpp
+++ LinearEngine.cpp
@@ -1,17 +1,17 @@
 #include <iostream>
 #include <vector>
-using namespace std;
+//using namespace std;
 
-vector<vector<int> > makeMatrix(vector<string> equations);
-void printer(vector<string> equations);
+std::vector<std::vector<int> > makeMatrix(std::vector<std::string> equations);
+void printer(std::vector<std::string> equations);
 
 int main(){
-        vector<string> equations;
-        string equation;
+        std::vector<string> equations;
+        std::string equation;
 
         for(int i=0;i<10;i++){
-                cout << "Enter equation " + to_string(i + 1) + " / click Enter 0 to complete." << endl;
-                cin >> equation;
+                std::cout << "Enter equation " + to_string(i + 1) + " / click Enter 0 to complete.\n";
+                std::cin >> equation;
                 if(equation != "0"){
                         equations.push_back(equation);
                 }else{
@@ -25,15 +25,15 @@ int main(){
 }
 
 
-vector<vector<int> > makeMatrix(vector<string> equations){
+std::vector<std::vector<int> > makeMatrix(std::vector<std::string> equations){
         //code for creating a 2d integer matrix from a 1d string vector.
-        vector<vector<int> > placeholder;
+        std::vector<std::vector<int> > placeholder;
         return placeholder;
 }
 
-void printer(vector<string> equations){
+void printer(std::vector<std::string> equations){
         for(int i=0;i<equations.size();i++){
-                cout << equations[i] << endl;
+                std::cout << equations[i] << "\n";
         }
 }
 
